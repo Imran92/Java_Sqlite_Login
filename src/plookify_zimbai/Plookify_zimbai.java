dcd4e055aee7f75cc63a5a9026ff07e7dc035da8
Imran <imranh920@gmail.com>
2016-06-13 16:49:59 +0600

    login and data saving working perfectly

---
+++ src/plookify_zimbai/Plookify_zimbai.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plookify_zimbai;
+
+/**
+ *
+ * @author Abdul-Malek
+ */
+public class Plookify_zimbai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Authenticate at = new Authenticate();
+        at.setVisible(true);
+    }
+    
+}
